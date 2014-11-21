cb854a94c4fa4f989c5aee6382d0a39aca97d033
Derril Lucci <derril.lucci@gmail.com>
2014-11-21 16:20:59 -0500

    Front end is done.  Now to hook up the pipes!

--- app/src/main/java/com/dlucci/businesscard/CardActivity.java
+++ app/src/main/java/com/dlucci/businesscard/CardActivity.java
@@ -1,17 +1,54 @@
 package com.dlucci.businesscard;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class CardActivity extends Activity {
+public class CardActivity extends Activity implements OnClickListener {
+
+    private EditText firstName, lastName, email, phoneNumber;
+    private Button start, stop;
+
+    private static String fName, lName, pEmail, pNumber;
+
+    private static Notification.Builder builder;
+    private static NotificationManager notification;
+
+    private static PendingIntent pIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.email);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+
+        start = (Button)findViewById(R.id.start);
+        stop = (Button) findViewById(R.id.stop);
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+        builder = new Notification.Builder(this).setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Business Card")
+                .setContentText("Send information")
+                .addAction(R.drawable.ic_launcher, "Text", pIntent)
+                .addAction(R.drawable.ic_launcher, "Email", pIntent)
+                .setOngoing(true);
+        notification = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
     }
 
 
@@ -33,4 +70,21 @@ public class CardActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == start.getId()){
+            fName = firstName.getText().toString();
+            lName = lastName.getText().toString();
+            pEmail = email.getText().toString();
+            pNumber = phoneNumber.getText().toString();
+            notification.notify(1, builder.build());
+        } else if(id == stop.getId()){
+            notification.cancel(1);
+        }
+
+    }
+
+    //private class
 }
